bb065b5a9a5a6d9eb623ba709a2b92d9cacaeeca
minh.dang4 <minh.dang4@mservice.com.vn>
2023-05-04 15:16:00 +0700

    Model for list items and associated migration

--- functional_tests.py
+++ functional_tests.py
@@ -1,12 +1,23 @@
 from selenium import webdriver
 import unittest
+#from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+import time
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
+        #self.browser = webdriver.Chrome(ChromeDriverManager().install())
         self.browser = webdriver.Firefox()
 
     def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows],
+            f"New to-do item did no appear in table. Contents were:\n{table.text}"
+        )
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # User has heard about a cool new online to-do app. She goes 
@@ -15,8 +26,9 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices the page title and header mention to-do lists.
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1')
+        header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
+
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
@@ -32,20 +44,18 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER) 
         time.sleep(1)
-        
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did no appear in table"
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers') 
 
-        self.fail('Finish the test!')
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        
         # The page updates again, and now shows both items on her list
-        
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers') 
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly') 
+        self.fail('Finish the test!')
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
